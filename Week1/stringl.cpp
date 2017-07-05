835dbf7ed2d4c4c402d808a29f5ae0e0218e2b9f
seekaddo <dennis_addo@aol.com>
2017-07-05 22:47:52 +0200

    Little practice and tricks with cpp

--- Week1/stringl.cpp
+++ Week1/stringl.cpp
@@ -1,6 +1,13 @@
-//
-// Created by Addo Dennis on 02/07/2017.
-//
+/* =============================================================================== */
+/* CPP Programming (Summer2017)                                                    */
+/* Created by Addo Dennis on 05/07/2017.                                           */
+/* =============================================================================== */
+/* Little playground with CPP. Is all mixture                                      */
+/*                                                                                 */
+/* Tested with g++ 6.3.0 , You can use clang++                                     */
+/* =============================================================================== */
+/* Author: Dennis Addo                                                             */
+/* =============================================================================== */
 
 #include <iostream>
 #include <iomanip>
@@ -15,7 +22,7 @@ using std::string;
 
 
 bool is_palindrome(const string& str){
-    
+
     return std::equal(str.begin(),str.end(),str.rbegin());  //remove_copy_if remove_copy
 }
 
@@ -31,60 +38,60 @@ bool smallnum(const int& s){
 
 
 int main() {
-    
-    
+
+
     string s = "eye";
-    
+
     cout << "The string: "+ s + (is_palindrome(s)?" is a palindrome":" is not palindrome") << '\n' <<endl;
-    
-    
+
+
     //std::vector<string> words = {"food","girls", "cat","dogs","mi","ku","y"};
     std::vector<int> nums = {3,4,6,7,8,2,9,13,45,87};
     std::sort(nums.begin(),nums.end(),std::less<int>());  // sort with smaller first
-    
+
     //std::remove_copy_if(words.begin(),words.end(),words.begin(),evenNum);
-    
+
     //returns a pointer to the last olds / false predicate
     std::vector<int>::iterator rel = std::stable_partition(nums.begin(), nums.end(), evenNum);
-    
+
     for (std::vector<int>::iterator i = nums.begin(); i != nums.end() ; ++i) {
         cout << *i <<endl;
     }
-    
+
     //extract from the old bis the end of the whole nums
     std::vector<int> olds(rel,nums.end());
-    
+
     //remove from the old pointer to the end
     nums.erase(rel,nums.end());
-    
+
     cout << "old nums: " <<endl;
     for (std::vector<int>::iterator i = olds.begin(); i != olds.end() ; ++i) {
         cout << *i <<endl;
     }
-    
-    
-    
+
+
+
     cout << "After removing olds numbers" <<endl;
     for (std::vector<int>::iterator i = nums.begin(); i != nums.end() ; ++i) {
         cout << *i <<endl;
     }
-    
-    
+
+
     std::vector<int> n = {3,4,6,7,8,2,9,13,45,87};
     std::vector<int> b;
-    
-    
+
+
     std::remove_copy_if(n.begin(),n.end(),back_inserter(b),smallnum); // don't store return value if you use back_inserter
-    
-    
+
+
     cout << "big numbers" <<endl;
     for (std::vector<int>::iterator i = b.begin(); i != b.end() ; ++i) {
         cout << *i <<' ';
     }
-    
+
     cout <<endl;
-    
-    
+
+
     return 0;
 }
 
